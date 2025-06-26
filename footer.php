418ed09ab2555774956cb80e4cd7838997a7d299
tomibady <tomibady@gmail.com>
2025-06-26 23:09:31 +0000

    Footer Frontend Dev

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
+<?php get_template_part('templates/partials/footer-main'); ?>
+
 <?php wp_footer(); ?>
 </body>
 </html>
--- templates/partials/top-notification.php
+++ templates/partials/top-notification.php
@@ -1,5 +1,5 @@
 <div class="bg-black">
     <div class="container px-4 py-2 mx-auto text-center text-white">
-        <p class="text-sm font-manrope">Sign up and get 20% off to your first order. <a href="#" class="underline">Sign Up Now</a></p>
+        <p class="text-sm font-manrope">Sign up and get 20% off to your first order. <a href="#" class="underline hover:text-woolemon">Sign Up Now</a></p>
     </div>
 </div>
